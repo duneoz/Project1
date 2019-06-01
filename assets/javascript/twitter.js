bf7f00c904c9ecac5b40dcc776cf629cf1250803
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-06-01 12:53:21 -0700

    full height twitter timeline in social feed

--- assets/javascript/twitter.js
+++ assets/javascript/twitter.js
@@ -7,7 +7,7 @@ function setTweets(brewery){
     newTimeline.addClass("twitter-timeline");
     newTimeline.attr("href", brewQuery);
     newTimeline.attr("data-width","100%");
-    newTimeline.attr("data-height","100%");
+    newTimeline.attr("data-height","440px");
     newTimeline.attr("data-chrome","noheader nofooter");
     $(".twitter-timeline").text("Tweets by @"+brewery);
     $("#twitterHolder").html(newTimeline);
